e3fa6ba6019c32e54db281ed16bf9043cfc4a709
Egor-Gaidiuchenko <evgaidiuchenko@gmail.com>
2023-02-08 09:19:45 +0600

    finish markup 2.0

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,35 +1,37 @@
-import React, {useState, useEffect} from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
 
 import './header.scss';
 
 const Header = () => {
-    const [links, showLinks] = useState(false);
+    const toggleLinks = () => {
+        const links = document.querySelector('.header__links');
 
-    if (window.innerWidth > 768) {
-        useEffect(() => {
-            showLinks(true);
-        });
-    }
+        links.classList.toggle('header__links--active');
+    };
 
-    const linksList = links ? 
-                    <div className='header__links header__links--active'>
-                        <Link to={'/mercury'} onClick={() => {showLinks(false);}} className='header__link'>Mercury</Link>
-                        <Link to={'/venus'} onClick={() => {showLinks(false);}} className='header__link'>Venus</Link>
-                        <Link to={'/earth'} onClick={() => {showLinks(false);}} className='header__link'>Earth</Link>
-                        <Link to={'/mars'} onClick={() => {showLinks(false);}} className='header__link'>Mars</Link>
-                        <Link to={'/jupiter'} onClick={() => {showLinks(false);}} className='header__link'>Jupiter</Link>
-                        <Link to={'/saturn'} onClick={() => {showLinks(false);}} className='header__link'>Saturn</Link>
-                        <Link to={'/uranus'} onClick={() => {showLinks(false);}} className='header__link'>Uranus</Link>
-                        <Link to={'/neptune'} onClick={() => {showLinks(false);}} className='header__link'>Neptune</Link>
-                    </div> : 
-                    null;
+    const removeLinks = () => {
+        if (window.innerWidth <= 768) {
+            const links = document.querySelector('.header__links');
+
+            links.classList.remove('header__links--active');
+        }
+    };
 
     return (
         <div className="header">
-            <Link to={'/'} className='header__logo'>the planets</Link>
-            <button className='header__burger' onClick={links ? () => {showLinks(false);} : () => {showLinks(true);}}></button>
-            {linksList}
+            <Link to={'/'} onClick={() => {removeLinks();}} className='header__logo'>the planets</Link>
+            <button className='header__burger' onClick={() => {toggleLinks();}}></button>
+            <div className='header__links header__links--active'>
+                <Link to={'/mercury'} onClick={() => {removeLinks();}} className='header__link'>Mercury</Link>
+                <Link to={'/venus'} onClick={() => {removeLinks();}} className='header__link'>Venus</Link>
+                <Link to={'/earth'} onClick={() => {removeLinks();}} className='header__link'>Earth</Link>
+                <Link to={'/mars'} onClick={() => {removeLinks();}} className='header__link'>Mars</Link>
+                <Link to={'/jupiter'} onClick={() => {removeLinks();}} className='header__link'>Jupiter</Link>
+                <Link to={'/saturn'} onClick={() => {removeLinks();}} className='header__link'>Saturn</Link>
+                <Link to={'/uranus'} onClick={() => {removeLinks();}} className='header__link'>Uranus</Link>
+                <Link to={'/neptune'} onClick={() => {removeLinks();}} className='header__link'>Neptune</Link>
+            </div>
         </div>  
     );
 };
